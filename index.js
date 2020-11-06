0f587270106f684cc471176371bb59bd9b68c93c
nickfern <nickfernandez1999@gmail.com>
2020-11-05 18:15:05 -0700

    10.2 10/29 /signup

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const db = require('./dbConnectExec.js')
-const app = express();
 
+const app = express();
+app.use(express.json())
 
 app.get("/hi",(req,res)=>{
     res.send("hello world")
 })
 
+app.post("/contacts", async (req,res)=>{
+    //res.send("creating user")
+    //console.log("request body", req.body)
+
+    var nameFirst = req.body.nameFirst;
+    var nameLast = req.body.nameLast;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    if(!nameFirst || !nameLast || !email || !password){
+        return res.status(400).send("bad request")
+    }
+
+    nameFirst = nameFirst.replace("'","''")
+    nameLast = nameLast.replace("'","''")
+
+    var emailCheckQuery = `SELECT email
+    from contact 
+    where email = '${email}'`
+
+    var existingUser = await db.executeQuery(emailCheckQuery)
+
+    var hashedPassword = bcrypt.hashSync(password)
+    //console.log("existing user", existingUser)
+    if(existingUser[0]){
+        return res.status(409).send('Please enter a different email.')
+    }
+
+    var insertQuery = `insert into contact(NameFirst,NameLast,Email,Password)
+    values('${nameFirst}','${nameLast}','${email}','${hashedPassword}')`
+
+    db.executeQuery(insertQuery)
+    .then(()=>{res.status(201).send()})
+    .catch((err)=>{
+        console.log("error in POST /contacts",err)
+        res.status(500).send()
+    })
+})
 
 app.get("/movies",(req,res)=>{
     //get data from database
---
+++ sandbox/hashing.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcryptjs')
+
+
+var hashedPassword = bcrypt.hashSync('asdfasdf')
+
+console.log(hashedPassword)
+
+var hashTest = bcrypt.compareSync('asdfasdf', hashedPassword)
+console.log(hashTest)
