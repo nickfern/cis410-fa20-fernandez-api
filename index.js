0d7b84dad97e30ac278bcfa6808bd211ae1a85f3
nickfern <nickfernandez1999@gmail.com>
2020-11-16 23:25:35 -0700

    deployment prep

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
 const db = require('./dbConnectExec.js')
 const config = require('./config.js')
 const auth = require('./middleware/authenticate')
 
+
+//azurewebsites.net, colostate.edu
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 
 
@@ -187,4 +192,6 @@ app.get("/movies/:pk", (req,res)=>{
             res.status(500).send()
         })
 })
-app.listen(5000,()=>{console.log("app is running on port 5000")})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{console.log(`app is running on port ${PORT}`)})
